615f74818e7087a69feee8867566661f6937387a
github username <github email address>
2019-07-06 22:43:26 -0400

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,13 +10,6 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
-commands = {
-  help=>'displays this help message',
-  list=>'displays a list of songs you can play',
-  play=>'lets you choose a song to play',
-  exit=>'exits the program'
-}
-
 # def say_hello(name)
 #   "Hi #{name}!"
 # end
@@ -26,11 +19,13 @@ commands = {
  
 # puts say_hello(users_name)
 
-def request_command
-  puts "Please Enter a Command:"
-end
-
-def help(commands)
+def help
+  commands = {
+  'help'=>'displays this help message',
+  'list'=>'displays a list of songs you can play',
+  'play'=>'lets you choose a song to play',
+  'exit'=>'exits the program'
+  }
   puts "Acceptable commands include the following:"
   commands.each{|k,v| puts "- #{k} : #{v}"}
 end
@@ -40,18 +35,40 @@ def list(songs)
 end
 
 def play(songs)
-  puts "Please enter a song name or number:"
   resp=gets.chomp
-  if songs[resp]
-    puts "Playing #{songs[resp]}"
-  elsif x=songs.find{|s| s.include?(resp)}
-    puts "Playing #{x}"
+  if resp.to_i-1>=0 && songs[resp.to_i-1]
+    # binding.pry
+    puts songs[resp.to_i-1]
+  elsif songs.include?(resp)
+    # binding.pry
+    puts songs.find{|s| s.include?(resp)}
   else
     puts 'Invalid input, please try again'
-    play(songs)
   end
 end
 
 def exit_jukebox
   puts 'Goodbye'
+  return
+end
+
+def run(songs)
+  help()
+  
+  puts "Please Enter a Command:"
+  input=gets.chomp()
+  
+  if input=='help'
+    help()
+  elsif input=='list'
+    list(songs)
+  elsif input=='play'
+    puts "Please enter a song name or number:"
+    play(songs)
+  elsif input=='exit'
+    exit_jukebox()
+  else
+    run(songs)
+  end
+
 end
